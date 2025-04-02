c173ee2a1b146baf7ac0e29d05a43d798b1b56de
umsuhyun2 <umsuhyun24@ewha.ac.kr>
2025-04-02 18:54:33 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 7272 ms, Memory: 757212 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+import sys
+input = sys.stdin.readline
+
+N, L = map(int, input().split())
+mydeque = deque()
+now = list(map(int, input().split()))
+
+for i in range(N):
+    while mydeque and mydeque[-1][0] > now[i]:
+        mydeque.pop()
+    mydeque.append((now[i], i))
+    
+    if mydeque[0][1] <= i - L:  # 범위를 벗어난 값 제거
+        mydeque.popleft()
+    
+    print(mydeque[0][0], end=' ')
